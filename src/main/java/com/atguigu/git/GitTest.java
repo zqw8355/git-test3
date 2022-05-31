88950cd2bd6cd80bbcaee4e19ab052b6a72bb115
zqw <835548329@qq.com>
2022-06-01 02:14:13 +0800

    three  master

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hot-fix first");
         System.out.println("ddd");
+        System.out.println("master-fix");
     }
 }
